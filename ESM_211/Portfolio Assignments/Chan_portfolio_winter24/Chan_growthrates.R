764bd74a5bd5b4a640dd962f5e1049b9c5b682b8
opossumpals <deloresjchan@ucsb.edu>
2024-02-02 11:19:51 -0800

    bleh

---
+++ ESM_211/Portfolio Assignments/Chan_portfolio_winter24/Chan_growthrates.R
@@ -0,0 +1,47 @@
+
+# Install and load packages
+# Install.packages("growthrates")
+library(growthrates)
+library(tidyverse)
+library(ggplot2)
+library(here)
+
+## Call in data 
+bison <- read.csv(here("Portfolio 3 - growthrates", "Data", "bison.csv"))
+summary(bison)
+
+bison_gg <- ggplot(bison, aes(x=year)) + 
+  geom_point(col="black",aes(y=bison),size=3) +
+  labs(x="Year", y="Bison Population (Counts)") +
+  ylim(0,5000) + xlim(1969,2000)+
+  theme_bw()
+bison_gg
+
+## 1. find r and K 
+
+## 1a. find exponential 
+
+bison_fit <- fit_easylinear(bison$year, bison$bison)
+coef(bison_fit)
+# r is 1.338e-01
+
+## 1b. logisitc 
+
+p <- c(y0 = 400, mumax = 1.338e-01, K = 5000)
+
+fit1 <- fit_growthmodel(FUN = grow_logistic, 
+                        p = p, 
+                        bison$year, 
+                        bison$bison)
+
+coef(fit1)
+
+plot(fit1, cex.lab = 2, cex.axis = 2)
+
+## 1c. spline 
+
+bison_spline <- fit_spline(bison$year, bison$bison)
+coef(bison_spline)
+
+## 2. do the different models agree on parameter values? 
+
---
+++ ESM_211/Portfolio Assignments/Chan_portfolio_winter24/Portfolio 4 - glarma/Code/Glarma.R
@@ -0,0 +1,188 @@
+## Glarma - DC
+####################
+
+install.packages("glarma")
+library(glarma)
+library(here)
+library(dplyr)
+library(tidyverse)
+
+## Read in data and add intercept
+bison <- read.csv(here("ESM_211", "Portfolio Assignments", "Glarma", "Data","bison.csv"))
+
+bison$intercept<-as.integer(1)
+
+## Create column for explanatory variables
+bison$major_events <- as.integer(0)
+
+bison$major_events[bison$year==1995] <- 1 # reintroduction of wolves to yellowstone NP
+
+bison$major_events[bison$year==1988] <- 1 # 1988 Yellowstone Fire
+  
+
+bison_gg <- ggplot(bison, aes(x=year)) + 
+  geom_point(col="black",aes(y=bison),size=3) +
+  labs(x="Year", y="Bison Population (Counts)") +
+  ylim(0,5000) + xlim(1969,2000)+
+  theme_bw()
+bison_gg
+
+
+
+## Identify variables
+y <- bison$bison # y value is bison counts
+x0 <- bison %>% select(intercept) # x0 is the null
+x0 <- as.matrix(x0) # turns x0 into a matrix 
+x1 <- bison %>% select(intercept, major_events) # x1 explanatory variables, presence of major events
+x1 <- as.matrix(x1) #turns x1 into a matrix
+
+
+
+## Null bison model with phiLag=1 
+null_bison_mod_lag1 <- glarma(y, x0, phiLags = c(1), type = "Poi", method = "FS",
+                    residuals = "Pearson", maxit = 100, grad = 1e-6)
+null_bison_mod_lag1
+
+summary(null_bison_mod_lag1)
+
+#plot.glarma(null_bison_mod_lag1)
+
+
+## Bison model with explanatory variables and phiLag=1
+exp_bison_mod_lag1 <- glarma(y, x1, phiLags = c(1), type = "Poi", method = "FS",
+                              residuals = "Pearson", maxit = 100, grad = 1e-6)
+exp_bison_mod_lag1
+
+summary(exp_bison_mod_lag1)
+
+#plot.glarma(exp_bison_mod_lag1)
+
+
+
+## Null bison glarma model with phiLag=2
+null_bison_mod_lag2 <- glarma(y, x0, phiLags = c(2), type = "Poi", method = "FS",
+                         residuals = "Pearson", maxit = 100, grad = 1e-6)
+null_bison_mod_lag2
+
+summary(null_bison_mod_lag2)
+
+#plot.glarma(null_bison_mod_lag2)
+
+
+## Bison model with explanatory variables and phiLag=2
+exp_bison_mod_lag2 <- glarma(y, x1, phiLags = c(2), type = "Poi", method = "FS",
+                             residuals = "Pearson", maxit = 100, grad = 1e-6)
+exp_bison_mod_lag2
+
+summary(exp_bison_mod_lag2)
+
+#plot.glarma(exp_bison_mod_lag2)
+
+
+
+## Null bison glarma model with phiLag=7
+null_bison_mod_lag7 <- glarma(y, x0, phiLags = c(7), type = "Poi", method = "FS",
+                         residuals = "Pearson", maxit = 100, grad = 1e-6)
+null_bison_mod_lag7
+
+summary(null_bison_mod_lag7)
+
+#plot.glarma(null_bison_mod_lag7)
+
+
+## Bison model with explanatory variables and phiLag=7
+exp_bison_mod_lag7 <- glarma(y, x1, phiLags = c(7), type = "Poi", method = "FS",
+                             residuals = "Pearson", maxit = 100, grad = 1e-6)
+exp_bison_mod_lag7
+
+summary(exp_bison_mod_lag7)
+
+#plot.glarma(exp_bison_mod_lag7)
+
+
+
+## Modified bison plot with lag=1
+bison_mod_1 <- bison 
+
+bison_mod_1$est <- exp_bison_mod_lag1$fitted.values #adds fitted values
+
+#Plots the model (black) and the bison count data (gray)
+bison_plot_mod_1<-ggplot(bison_mod_1, aes(x=year)) + 
+  geom_point(col="gray",aes(y=bison),size=3) + 
+  geom_point(col="black",aes(y=est),size=3) +
+  geom_line(col="black",aes(y=est)) +
+  labs(x="Year", y="Bison Population") +
+  ylim(0,4000) + xlim(1970,1997)+
+  theme_bw()
+
+#Adds two red horizontal lines for the occurence of fire and reintroduction of wolves
+bison_plot_mod_1 <-bison_plot_mod_1 + 
+  annotate("rect", xmin = 1988, xmax =1988.1, ymin = 0, ymax = 4000, alpha = .75,fill = "red") + 
+  annotate("rect", xmin = 1995, xmax =1995.1, ymin = 0, ymax = 4000, alpha = .75,fill = "red")
+
+ggsave(here("ESM_211", "Portfolio Assignments", "Glarma", "plots","bison_plot_mod_1.jpg"), bison_plot_mod_1, dpi=500, width=6, height=3, unit="in")
+
+bison_plot_mod_1 
+#ERROR MESSAGE: Removed 1 rows containing missing values (`geom_point()`). -- Might just ignore this, I don't know what was removed. (DC)
+
+
+
+## Modified bison plot with lag=2
+bison_mod_2 <- bison 
+
+bison_mod_2$est <- exp_bison_mod_lag2$fitted.values #adds fitted values
+
+#Plots the model (black) and the bison count data (gray)
+bison_plot_mod_2<-ggplot(bison_mod_2, aes(x=year)) + 
+  geom_point(col="gray",aes(y=bison),size=3) + 
+  geom_point(col="black",aes(y=est),size=3) +
+  geom_line(col="black",aes(y=est)) +
+  labs(x="Year", y="Bison Population") +
+  ylim(0,4000) + xlim(1970,1997)+
+  theme_bw()
+
+#Adds two red horizontal lines for the occurence of fire and reintroduction of wolves
+bison_plot_mod_2 <-bison_plot_mod_2 + 
+  annotate("rect", xmin = 1988, xmax =1988.1, ymin = 0, ymax = 4000, alpha = .75,fill = "red") + 
+  annotate("rect", xmin = 1995, xmax =1995.1, ymin = 0, ymax = 4000, alpha = .75,fill = "red")
+
+ggsave(here("ESM_211", "Portfolio Assignments", "Glarma", "plots","bison_plot_mod_2.jpg"), bison_plot_mod_2, dpi=500, width=6, height=3, unit="in")
+
+bison_plot_mod_2
+#ERROR MESSAGE: Removed 1 rows containing missing values (`geom_point()`). -- Might just ignore this, I don't know what was removed. (DC)
+
+
+
+## Modified bison plot with lag=7
+bison_mod_7 <- bison 
+
+bison_mod_7$est <- exp_bison_mod_lag7$fitted.values #adds fitted values
+
+#Plots the model (black) and the bison count data (gray)
+bison_plot_mod_7<-ggplot(bison_mod_7, aes(x=year)) + 
+  geom_point(col="gray",aes(y=bison),size=3) + 
+  geom_point(col="black",aes(y=est),size=3) +
+  geom_line(col="black",aes(y=est)) +
+  labs(x="Year", y="Bison Population") +
+  ylim(0,4100) + xlim(1970,1997)+
+  theme_bw()
+
+#Adds two red horizontal lines for the occurence of fire and reintroduction of wolves
+bison_plot_mod_7 <-bison_plot_mod_7 + 
+  annotate("rect", xmin = 1988, xmax =1988.1, ymin = 0, ymax = 4100, alpha = .75,fill = "red") + 
+  annotate("rect", xmin = 1995, xmax =1995.1, ymin = 0, ymax = 4100, alpha = .75,fill = "red")
+
+ggsave(here("ESM_211", "Portfolio Assignments", "Glarma", "plots","bison_plot_mod_7.jpg"), bison_plot_mod_7, dpi=500, width=6, height=3, unit="in")
+
+bison_plot_mod_7 
+#ERROR MESSAGE: Removed 1 rows containing missing values (`geom_point()`). -- Might just ignore this, I don't know what was removed. (DC)
+
+
+
+summary(null_bison_mod_lag1) #AIC = 7200.342
+summary(null_bison_mod_lag2) #AIC = 6684.275
+summary(null_bison_mod_lag7) #AIC = 14214.01
+
+summary(exp_bison_mod_lag1) #AIC = 5235.738
+summary(exp_bison_mod_lag2) #AIC = 6265.411
+summary(exp_bison_mod_lag7) #AIC = 12944.64
