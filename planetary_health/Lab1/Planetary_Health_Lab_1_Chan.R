eeb0db52c01241e9046e164296acda970204fb79
opossumpals <deloresjchan@ucsb.edu>
2024-04-06 13:31:34 -0700

    completed

--- planetary_health/Lab1/Planetary_Health_Lab_1_Chan.R
+++ planetary_health/Lab1/Planetary_Health_Lab_1_Chan.R
@@ -54,6 +54,34 @@ den_area <- Den %>% group_by(District) %>%
         # Piura has a population of 484,475 in 2017 -- this is not one of the largest cities in Peru. 
 
 # 3) Find another health dataset and repeat the above (could be any health outcome and in any place)
+        # This dataset does not contain times of the year when asthma hospitalizations spike, but in 2020, asthma hospitalizations were
+        # much higher. Los Angeles County has the highest number of hospitalizations with 14,086 from 2015-2020. This is a highly populated
+        # urban area that is characterized by heavy industrial and urban pollution. This is not surprising as heavy polluted areas have 
+        # poor air quality which can result in higher rates of asthma. 
+
+# read in asthma data 
+ca_asthma <- read.csv("asthma-hospitalization-rates-by-county-2015_2020.csv")
+head(ca_asthma)
+lapply(ca_asthma, class)
+
+# make number of hospitalizations numeric
+ca_asthma$Number_hospitalization <- as.numeric(ca_asthma$NUMBER.OF.HOSPITALIZATIONS)
+
+ca_asthma_cleaned <- na.omit(ca_asthma)
+
+# plot for # of asthma hospitalizations from 2015-2020 
+ca_asthma_plot <- ggplot(ca_asthma_cleaned, aes(x=YEAR, y=Number_hospitalization, col=as.factor(COUNTY))) +
+  geom_point() + 
+  theme(legend.position="none")
+ca_asthma_plot 
+
+# group and filter to find county with highest number of asthma hospitalization 
+# Using > 500 because prior to 2020, the highest number of asthma hospitalization was around 1000. 
+ca_asthma_area <- ca_asthma_cleaned %>% group_by(COUNTY) %>% 
+  filter(Number_hospitalization > 500, na.rm=TRUE) %>% 
+  summarize(total_county_hospitalization = sum(Number_hospitalization))
+
+
 
 # https://libguides.tulane.edu/c.php?g=182563&p=1354703
 # CDC WONDER: https://wonder.cdc.gov/controller/datarequest/D140
